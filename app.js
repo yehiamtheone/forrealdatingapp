e3ed9f6ff289db828d0630c8698b115991fa768b
yehiamtheone <yehiamknafo5757@gmail.com>
2025-06-17 14:20:45 +0300

    fixed db cluster + db env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectToDB = require('./db/mongoConnect.js');
 const Routes = require('./routes/config_routes.js');
 const { EXPRESS_PORT} = require('./secret/secretConf.js');
-require("./server.js");
+require("./SocketServer.js");
 
 // Initialize Express app
 const app = express();
